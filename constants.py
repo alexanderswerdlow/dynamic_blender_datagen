1bdd3ca1826494fc470cc24d7e83c42e75f4c15d
Alexander Swerdlow <aswerdlow1@gmail.com>
2024-05-25 11:39:19 -0400

    update

--- constants.py
+++ constants.py
@@ -2,4 +2,7 @@ from pathlib import Path
 import os
 
 DATA_DIR = Path.home() / "data" / "point_odyssey" / "data"
-DATA_DIR = os.getenv("DATA_DIR", DATA_DIR)
+if DATA_DIR.exists() is False:
+    DATA_DIR = Path("data")
+
+DATA_DIR = Path(os.getenv("DATA_DIR", DATA_DIR))
